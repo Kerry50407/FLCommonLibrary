00f68ead78607591a74b1ba12b395ab816ad47ea
KerryDong <kerrydong@taiwanmobile.com>
2019-06-18 11:11:45 +0800

    Setup pod spec library

---
+++ Example/FLCommonLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FLCommonLibrary
+//
+//  Created by KerryDong on 06/18/2019.
+//  Copyright (c) 2019 KerryDong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ SampleClass.swift
@@ -0,0 +1,12 @@
+//
+//  SampleClass.swift
+//  FLCommonLibrary
+//
+//  Created by Kerry Dong on 2019/6/18.
+//
+
+import Foundation
+
+class SampleClass {
+    
+}
